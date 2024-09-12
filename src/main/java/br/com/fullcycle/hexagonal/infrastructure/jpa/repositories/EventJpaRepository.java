3c1efd33ddc71d9efbbfd8b7e2b8fb2d1519d45d
Leonardo Marques <leomarques301@gmail.com>
2024-09-11 21:33:47 -0300

    split project into sub-modules

--- src/main/java/br/com/fullcycle/hexagonal/infrastructure/jpa/repositories/EventJpaRepository.java
+++
@@ -1,9 +0,0 @@
-package br.com.fullcycle.hexagonal.infrastructure.jpa.repositories;
-
-import br.com.fullcycle.hexagonal.infrastructure.jpa.entities.EventEntity;
-import io.hypersistence.tsid.TSID;
-import org.springframework.data.repository.CrudRepository;
-
-public interface EventJpaRepository extends CrudRepository<EventEntity, TSID> {
-
-}
